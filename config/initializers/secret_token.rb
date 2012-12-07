3dc96bd879a2008edb63545bd67f04628de48a8a
James Muranga <jmured@gmail.com>
2012-12-07 22:55:03 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cedres::Application.config.secret_token = '9b762362dcecf00818125cec43199d11d1887c44b1d7532c34044cc741947259fe0c98a9430d58f351f25e175f4509b714e9e25a889dc0bbc3aae1815c85f4b6'
